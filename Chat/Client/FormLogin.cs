819c9231ff21984ea8384c5afd42bd68714c0029
Patrik I <260010@via.dk>
2018-11-19 17:24:31 +0100

    Register user reworked to ADO.NET

--- Chat/Client/FormLogin.cs
+++ Chat/Client/FormLogin.cs
@@ -8,48 +8,156 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace Client
 {
     public partial class FormLogin : Form
     {
 
+
         public FormLogin()
         {
             InitializeComponent();
         }
+        private bool isPasswordOk()
+        {
+            if (PasswordTextbox.Text == "")
+            {
+                MessageBox.Show("Please enter valid name.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
+        private bool isNameOk()
+        {
+            if (NameTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter valid name.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void ClearForm()
+        {
+            NameTextBox.Clear();
+            PasswordTextbox.Clear();
+        }  
+        
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            /* MainForm mainForm = new MainForm();
+             FormLogin loginForm = new FormLogin();
+
+             //Login authentificator
+             SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\School\\DNP1\\ProjectDNP\\DNP_Chat\\Chat\\Client\\Database1.mdf;Integrated Security=True");
+             con.Open();
+             SqlDataAdapter sda = new SqlDataAdapter("select count (*) from Users where loginName ='" + NameTextBox.Text + "' and password ='" + PasswordTextbox.Text + "'", con);
+             DataTable dt = new DataTable();
+             sda.Fill(dt);
+             if (dt.Rows[0][0].ToString() == "1")
+             {
+                 mainForm.Show();
+                 this.Hide();
+             }
+             else
+                 MessageBox.Show("USER DOES NOT EXIST","Error",MessageBoxButtons.OK,MessageBoxIcon.Error); */
+
             MainForm mainForm = new MainForm();
             FormLogin loginForm = new FormLogin();
 
-            //Login authentificator
-            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\School\\DNP1\\ProjectDNP\\DNP_Chat\\Chat\\Client\\Database1.mdf;Integrated Security=True");
-            con.Open();
-            SqlDataAdapter sda = new SqlDataAdapter("select count (*) from Users where loginName ='" + NameTextBox.Text + "' and password ='" + PasswordTextbox.Text + "'", con);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
-            if (dt.Rows[0][0].ToString() == "1")
+            if (isPasswordOk() && isNameOk())
             {
-                mainForm.Show();
-                this.Hide();
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+                {
+                    //const String sql = "SELECT * FROM UsersInfo WHERE loginName = @LoginName AND password = @Password";
+
+                    //using (SqlCommand sqlCommand = new SqlCommand(sql, connection))
+                    //{
+                       // sqlCommand.Parameters.Add(new SqlParameter("@LoginName", SqlDbType.VarChar, 50));
+                       // sqlCommand.Parameters["@LoginName"].Value = NameTextBox.Text;
+
+                       // sqlCommand.Parameters.Add(new SqlParameter("@Password", SqlDbType.VarChar, 50));
+                       // sqlCommand.Parameters["@Password"].Value = PasswordTextbox.Text;
+
+                        try
+                        {
+                            connection.Open();
+                            SqlDataAdapter sda = new SqlDataAdapter("select count (*) from Users where loginName ='" + NameTextBox.Text + "' and password ='" + PasswordTextbox.Text + "'", connection);
+                            DataTable dt = new DataTable();
+                            sda.Fill(dt);
+                            
+                            if (dt.Rows[0][0].ToString() == "1")
+                            {
+                                mainForm.Show();
+                                this.Hide();
+                                MessageBox.Show("Logged in.");
+                            }
+                            else
+                            {
+                            MessageBox.Show("USER DOES NOT EXIST", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                        catch
+                        {
+                            MessageBox.Show("USER DOES NOT EXIST", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            NameTextBox.Clear();
+                            PasswordTextbox.Clear();
+                            connection.Close();
+                        }
+                    //}
+                } 
             }
-            else
-                MessageBox.Show("USER DOES NOT EXIST","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
         }
 
         private void RegisterButton_Click(object sender, EventArgs e)
         {
-            //Register user
-            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\School\\DNP1\\ProjectDNP\\DNP_Chat\\Chat\\Client\\Database1.mdf;Integrated Security=True");
-            con.Open();
-            SqlCommand sc = new SqlCommand ("INSERT INTO Users values('"+ NameTextBox.Text + "','"+PasswordTextbox.Text+"');",con);
-            object o = sc.ExecuteNonQuery();
-            MessageBox.Show(o + ": New user has been added");
-            con.Close();
-            NameTextBox.Clear();
-            PasswordTextbox.Clear();
+            if (isPasswordOk() && isNameOk())
+            {
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand("NewLogin", connection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                        sqlCommand.Parameters.Add(new SqlParameter("@LoginName", SqlDbType.VarChar, 50));
+                        sqlCommand.Parameters["@LoginName"].Value = NameTextBox.Text;
+
+                        sqlCommand.Parameters.Add(new SqlParameter("@Password", SqlDbType.VarChar, 50));
+                        sqlCommand.Parameters["@Password"].Value = PasswordTextbox.Text;
+                        
+                        try
+                        {
+                            connection.Open();
+                            sqlCommand.ExecuteNonQuery();
+
+                        }
+                        catch (Exception e1)
+                        {
+                            MessageBox.Show("Invalid values", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            MessageBox.Show("New user has been added");
+                            NameTextBox.Clear();
+                            PasswordTextbox.Clear();
+                            connection.Close();
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
